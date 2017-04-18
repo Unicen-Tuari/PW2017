237fc7c6bcdae5989df142dfd5b1ad0275425282
Javier Dottori <javiermdq@gmail.com>
2017-04-18 14:45:20 -0300

    Generar dados y mostrarlos. Close #29

--- Dados/js/dados.js
+++ Dados/js/dados.js
@@ -1,3 +1,10 @@
+"use strict";
 function tirarDados() {
-  alert("Tiro dados");
+  let num1 = Math.floor(Math.random()*6+1);
+  let num2 = Math.floor(Math.random()*6+1);
+  let dado1 = document.getElementById("primerDado");
+  dado1.src = "images/dado"+num1+".png"
+  let dado2 = document.getElementById("segundoDado");
+  dado2.src = "images/dado"+num2+".png"
+  console.log(num1 + " - " + num2);
 }
