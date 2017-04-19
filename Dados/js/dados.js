82cd01e30dd36d588e1ef73919b03230f1666596
Ignacio Jonas <ignaciojonas@gmail.com>
2017-04-19 20:31:34 -0300

    Mostrar cuantas veces salio cada suma Closes #31

--- Dados/js/dados.js
+++ Dados/js/dados.js
@@ -1,10 +1,29 @@
 "use strict";
+
+let resultados = [0,0,0,0,0,0,0,0,0,0,0];
+const tds = document.querySelectorAll('#resultado td');
+const caras = 6;
+
+function obtenerValor(){
+  return Math.floor(Math.random() * caras + 1);
+}
+
 function tirarDados() {
-  let num1 = Math.floor(Math.random()*6+1);
-  let num2 = Math.floor(Math.random()*6+1);
+  let num1 = obtenerValor();
+  let num2 = obtenerValor();
   let dado1 = document.getElementById("primerDado");
   dado1.src = "images/dado"+num1+".png"
   let dado2 = document.getElementById("segundoDado");
   dado2.src = "images/dado"+num2+".png"
   console.log(num1 + " - " + num2);
+  let resultado = num1 + num2;
+  resultados[resultado - 2] += 1;
+  console.log(resultados);
+  mostrarResultados(resultados);
+}
+
+function mostrarResultados(resultados) {
+  for (let i = 0; i < tds.length; i++) {
+    tds[i].innerHTML = resultados[i];
+  }
 }
